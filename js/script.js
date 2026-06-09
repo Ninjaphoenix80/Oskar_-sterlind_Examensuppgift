578d1520ba68dd23fc7c03b60e9c50629dd0b781
Ninjaphoenix80 <n86462686@gmail.com>
2026-06-09 10:54:11 +0200

    Sixth Commit
    Allting grundläggande fungerar, bara extra valfria funktionerna saknas.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 document.addEventListener("keydown", (Event) =>{
-    
+    let PressedKey = document.getElementById(Event.code)
+    console.log(PressedKey)
+
+    PressedKey.style = "background-color: blue"
+})
+
+document.addEventListener("keyup", (Event) =>{
+    let PressedKey = document.getElementById(Event.code)
+    console.log(PressedKey)
+    PressedKey.style = "background-color: black"
 })
