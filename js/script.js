bdb900f5d28d631844c4fccc6053f366c2c1105f
Ninjaphoenix80 <n86462686@gmail.com>
2026-06-09 16:11:49 +0200

    Seventh commit
    I fixed a very small mistake.
    The color of pressed buttons are now the right color.
    I also changed the color of buttons that are pressed down because it looked better.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-document.addEventListener("keydown", (Event) =>{
-    let PressedKey = document.getElementById(Event.code)
-    console.log(PressedKey)
 
-    PressedKey.style = "background-color: blue"
+document.addEventListener("keydown", (Event) =>{
+    console.log(Event.code)
+
+    let PressedKey = document.getElementById(Event.code)
+    
+    PressedKey.style = "background-color: tomato"
 })
 
 document.addEventListener("keyup", (Event) =>{
     let PressedKey = document.getElementById(Event.code)
-    console.log(PressedKey)
-    PressedKey.style = "background-color: black"
+    console.log(event.code)
+    PressedKey.style = "background-color: rgb(26, 26, 26)"
 })
